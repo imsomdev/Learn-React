60d69e07956d062a754e00f9db0e27451bcb2cf8
imsomdev <47299585+imsomdev@users.noreply.github.com>
2024-04-16 19:23:39 +0530

    learned basics of react element and jsx

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-const heading = React.createElement(
-    "h1", 
-    {
-        id: "title"
-    }, 
-    "Hello World");
-// console.log(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+without JSX
+*/
+
+const heading_1 = React.createElement(
+  "h1",
+  {
+    id: "title",
+    key: "h1",
+  },
+  "Hello World"
+);
+const heading_2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+    key: "h2",
+  },
+  "Hi World"
+);
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading_1, heading_2]
+);
+
+// With JSX
+const heading = (
+  <h1 id="title" key="h1">
+    Hello Wolrd
+  </h1>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);

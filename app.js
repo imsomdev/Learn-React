8a8c29ce31571181ad473f1a3706c253f24dae57
imsomdev <47299585+imsomdev@users.noreply.github.com>
2024-04-17 15:53:49 +0530

    Learnt about react component and before comment

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*
-without JSX
-*/
+/**
+ * These are pure ReactElements
+ * But its readbility is low, so we won't use them.
+ */
 
 const heading_1 = React.createElement(
   "h1",
@@ -29,11 +30,49 @@ const container = React.createElement(
   [heading_1, heading_2]
 );
 
-// With JSX
+/**
+ * This is JSX. Which is compiled/ or converted into React Element by Bable.
+ * This HTML like syntax,, but not html
+ * Its like HTML inside JS.
+ */
 const heading = (
   <h1 id="title" key="h1">
     Hello Wolrd
   </h1>
 );
+
+/**
+ * This is a Functional Component
+ */
+const Title = () => (
+  <h1 id="title" key="h1">
+    This is Functional Component
+  </h1>
+);
+
+/**
+ * React Component is Nothing but a JavaScript function
+ * Convetion is To Start the With Caps(PascalCase, UpperCamelCase )
+ * Use => Functions, you can use normal function too
+ * Inside function you will write only JSX
+ */
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/* Inserting ReactElement inside a Component */}
+      {heading}
+
+      {/* Inserting Functional Component Inside Functional Component 
+      This is Also Called "ComponentComposition"*/}
+      {/* <Title /> */}
+      {Title()}
+      <h2>This is h1 Tag</h2>
+
+      {console.log("You can write any JS Code in side this {} braces")}
+      <h2>This is h2 Tag</h2>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(<HeaderComponent />);
